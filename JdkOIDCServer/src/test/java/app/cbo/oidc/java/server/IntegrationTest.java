99717d698ca92b2da363b00b308fb1f1dd50b021
CYRILLE BOUCHER <15726172+boucyr@users.noreply.github.com>
2023-05-10 16:05:17 +0200

    Refactoring of integration test

--- JdkOIDCServer/src/test/java/app/cbo/oidc/java/server/IntegrationTest.java
+++ JdkOIDCServer/src/test/java/app/cbo/oidc/java/server/IntegrationTest.java
@@ -4,10 +4,13 @@ import app.cbo.oidc.java.server.credentials.TOTP;
 import app.cbo.oidc.java.server.endpoints.authorize.AuthorizeHandler;
 import app.cbo.oidc.java.server.endpoints.jwks.JWKSHandler;
 import app.cbo.oidc.java.server.endpoints.token.TokenHandler;
+import app.cbo.oidc.java.server.endpoints.userinfo.UserInfoHandler;
 import app.cbo.oidc.java.server.utils.MimeType;
 import app.cbo.oidc.java.server.utils.QueryStringParser;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.JWKSet;
@@ -53,34 +56,93 @@ public class IntegrationTest {
                 .build();
 
 
-        var authorizeRequest = HttpRequest.newBuilder()
-                .uri(new URI(ROOT + AuthorizeHandler.AUTHORIZE_ENDPOINT + "?scope=openid&redirect_uri=http://cbo.app&response_type=code&client_id=INTEGRATION&nonce=monNonce"))
-                .GET().build();
-
-        HttpResponse<String> loginPage = followRedirects(browser, authorizeRequest);
-
-        assertThat(loginPage.statusCode()).isEqualTo(200);
-        var totp = TOTP.get("ALBACORE"); //[05/04/2023] should you an external way to generate TOTP
-        var loginOngoings = QueryStringParser.from(loginPage.uri().getQuery()).get("ongoing");
-        assertThat(loginOngoings)
-                .isNotNull()
-                .hasSize(1);
-        var loginOngoing = loginOngoings.iterator().next();
-
-
-        var authenticationRequest = HttpRequest.newBuilder()
-                .uri(loginPage.uri())
-                .header("Content-Type", MimeType.FORM.mimeType())
-                .POST(HttpRequest.BodyPublishers.ofString("login=cyrille&pwd=sesame&totp=" + totp + "&ongoing=" + loginOngoing))
-                .build();
-
-        var consentPage = followRedirects(browser, authenticationRequest);
+        HttpResponse<String> loginPage = callAuthorizeAndRedirectToLogin(browser);
+        HttpResponse<String> consentPage = submitCredentialsAndRedirectToConsents(browser, loginPage);
 
         String consentOngoing = readOngoingInputField(consentPage.body());
         assertThat(consentPage.statusCode()).isEqualTo(200);
 
-        assertThat(consentOngoing).isNotEqualTo(loginOngoing);
+        var code = submitConsentAndRedirectToClientWithCode(browser, consentPage, consentOngoing);
 
+
+        JsonNode json = submitCode(browser, code);
+
+        var accessToken = json.get("access_token");
+        var refreshToken = json.get("refresh_token");
+        var idToken = json.get("id_token");
+
+        assertThat(accessToken).isNotNull();
+        assertThat(refreshToken).isNotNull();
+        assertThat(idToken).isNotNull();
+
+        DecodedJWT decoded = validateJWT(idToken);
+
+
+        assertThat(decoded.getSubject()).isEqualTo("cyrille");
+        //TODO : calls userinfo endpoint
+        var userInfoRequest = HttpRequest.newBuilder()
+                .uri(new URI(ROOT + UserInfoHandler.USERINFO_ENDPOINT))
+                .header("Content-Type", MimeType.FORM.mimeType())
+                .header("Authorization", "Bearer " + accessToken.asText())
+                .POST(HttpRequest.BodyPublishers.noBody())
+                .build();
+        var userInfoResponse = browser.send(userInfoRequest, HttpResponse.BodyHandlers.ofString());
+        assertThat(userInfoResponse).isNotNull();
+        assertThat(userInfoResponse.statusCode()).isEqualTo(200);
+        var userInfo = new ObjectMapper().reader().readTree(userInfoResponse.body());
+
+        userInfo.toString();
+        assertThat(userInfo).isNotNull();
+        assertThat(userInfo.get("name").asText()).isEqualToIgnoringCase("cyrille boucher");
+        assertThat(userInfo.get("phone")).isNull(); //did not ask for 'phone' scope
+
+    }
+
+    private DecodedJWT validateJWT(JsonNode idToken) throws JOSEException {
+        var decoded = JWT.decode(idToken.asText());
+        var keyId = decoded.getKeyId();
+
+        //TODO call jwks endpoint instead
+        JWKSet jwkSet;
+        try {
+            jwkSet = JWKSet.load(new URL(ROOT + JWKSHandler.JWKS_ENDPOINT));
+        } catch (Exception e) {
+            fail("cannot load JKWSet");
+            throw new RuntimeException("wnh");
+        }
+        var key = jwkSet.getKeys().stream().filter(jwk -> jwk.getKeyID().equals(keyId)).findFirst();
+        if (key.isEmpty()) {
+            fail("Cannot find signature key in keyset");
+            throw new RuntimeException("wnh");
+        }
+        var publicKey = key.get();
+
+        if (publicKey instanceof RSAKey rsaKey) {
+            var javaKey = (RSAPublicKey) rsaKey.toPublicKey();
+            JWT.require(Algorithm.RSA256(javaKey)).build().verify(decoded);
+        }
+        assertThat(decoded).isNotNull();
+        return decoded;
+    }
+
+    private JsonNode submitCode(HttpClient browser, String code) throws URISyntaxException, IOException, InterruptedException {
+        var codeReq = HttpRequest.newBuilder()
+                .uri(new URI(ROOT + TokenHandler.TOKEN_ENDPOINT))
+                .header("Content-Type", MimeType.FORM.mimeType())
+                .header("Authorization", "basic " + Base64.getEncoder().encodeToString("INTEGRATION:INTEGRATION".getBytes(StandardCharsets.UTF_8)))
+                .POST(HttpRequest.BodyPublishers.ofString("code=" + code + "&grant_type=authorization_code&client_id=INTEGRATION&redirect_uri=http://cbo.app"))
+                .build();
+        var codeRes = browser.send(codeReq, ofString());
+        var codeStatus = codeRes.statusCode();
+        assertThat(codeStatus).isEqualTo(200);
+
+        var tokenResponse = codeRes.body();
+
+        var json = new ObjectMapper().reader().readTree(tokenResponse);
+        return json;
+    }
+
+    private String submitConsentAndRedirectToClientWithCode(HttpClient browser, HttpResponse<String> consentPage, String consentOngoing) throws IOException, InterruptedException, URISyntaxException {
         var consentRequest = HttpRequest.newBuilder()
                 .uri(consentPage.uri())
                 .header("Content-Type", MimeType.FORM.mimeType())
@@ -107,64 +169,40 @@ public class IntegrationTest {
                 .isNotNull()
                 .isNotEmpty()
                 .hasSize(1);
+
         var code = codeParam.iterator().next();
-        var codeReq = HttpRequest.newBuilder()
-                .uri(new URI(ROOT + TokenHandler.TOKEN_ENDPOINT))
+        return code;
+    }
+
+    private HttpResponse<String> submitCredentialsAndRedirectToConsents(HttpClient browser, HttpResponse<String> loginPage) throws IOException, InterruptedException, URISyntaxException, OutsideRedirect {
+        var totp = TOTP.get("ALBACORE"); //[05/04/2023] should you an external way to generate TOTP
+        var loginOngoings = QueryStringParser.from(loginPage.uri().getQuery()).get("ongoing");
+        assertThat(loginOngoings)
+                .isNotNull()
+                .hasSize(1);
+        var loginOngoing = loginOngoings.iterator().next();
+
+
+        var encodeduri = new URI(loginPage.uri().toString().replace(" ", "%20"));
+        var authenticationRequest = HttpRequest.newBuilder()
+                .uri(encodeduri)
                 .header("Content-Type", MimeType.FORM.mimeType())
-                .header("Authorization", "basic " + Base64.getEncoder().encodeToString("INTEGRATION:INTEGRATION".getBytes(StandardCharsets.UTF_8)))
-                .POST(HttpRequest.BodyPublishers.ofString("code=" + code + "&grant_type=authorization_code&client_id=INTEGRATION&redirect_uri=http://cbo.app"))
+                .POST(HttpRequest.BodyPublishers.ofString("login=cyrille&pwd=sesame&totp=" + totp + "&ongoing=" + loginOngoing))
                 .build();
-        var codeRes = browser.send(codeReq, ofString());
 
-        var codeStatus = codeRes.statusCode();
+        var consentPage = followRedirects(browser, authenticationRequest);
+        return consentPage;
+    }
 
-        var tokenResponse = codeRes.body();
-        var json = new ObjectMapper().reader().readTree(tokenResponse);
-        var accessToken = json.get("access_token");
-        var refreshToken = json.get("refresh_token");
-        var idToken = json.get("id_token");
+    private HttpResponse<String> callAuthorizeAndRedirectToLogin(HttpClient browser) throws URISyntaxException, IOException, InterruptedException, OutsideRedirect {
+        var authorizeRequest = HttpRequest.newBuilder()
+                .uri(new URI(ROOT + AuthorizeHandler.AUTHORIZE_ENDPOINT + "?scope=openid%20profile&redirect_uri=http://cbo.app&response_type=code&client_id=INTEGRATION&nonce=monNonce"))
+                .GET().build();
 
-        assertThat(accessToken).isNotNull();
-        assertThat(refreshToken).isNotNull();
-        assertThat(idToken).isNotNull();
+        HttpResponse<String> loginPage = followRedirects(browser, authorizeRequest);
 
-        var decoded = JWT.decode(idToken.asText());
-        var keyId = decoded.getKeyId();
-
-        //TODO call jwks endpoint instead
-        JWKSet jwkSet;
-        try {
-            jwkSet = JWKSet.load(new URL(ROOT + JWKSHandler.JWKS_ENDPOINT));
-        } catch (Exception e) {
-            fail("cannot load JKWSet");
-            return;
-        }
-        var key = jwkSet.getKeys().stream().filter(jwk -> jwk.getKeyID().equals(keyId)).findFirst();
-        if (key.isEmpty()) {
-            fail("Cannot find signature key in keyset");
-            return;
-        }
-        var publicKey = key.get();
-
-        if (publicKey instanceof RSAKey rsaKey) {
-            var javaKey = (RSAPublicKey) rsaKey.toPublicKey();
-
-            JWT.require(Algorithm.RSA256(javaKey)).build().verify(decoded);
-        }
-
-
-        /*var pub = (RSAPublicKey) KeySet.getInstance().publicKey(KeyId.of(keyId)).orElseThrow();
-        var priv = (RSAPrivateKey) KeySet.getInstance().privateKey(KeyId.of(keyId)).orElseThrow();
-        JWT.require(Algorithm.RSA256(pub, priv))
-                .build().verify(decoded);*/
-
-        assertThat(decoded.getSubject()).isEqualTo("cyrille");
-        //TODO : calls userinfo endpoint
-//        var userInfoRequest = HttpRequest.newBuilder()
-//                .uri(new.uri())
-//                .header("Content-Type", MimeType.FORM.mimeType())
-//                .POST(HttpRequest.BodyPublishers.ofString("backFromForm=true&scope_openid=on&scope_profile=on&ongoing=" + consentOngoing))
-//                .build();
+        assertThat(loginPage.statusCode()).isEqualTo(200);
+        return loginPage;
     }
 
     private String readOngoingInputField(String html) {
@@ -190,7 +228,7 @@ public class IntegrationTest {
             var target = locationHeader.get();
 
             //fix relative redirects
-            var redirectedTo = target.startsWith("http") ? new URI(target) : new URI(ROOT + locationHeader.get());
+            var redirectedTo = target.startsWith("http") ? new URI(target.replace(" ", "%20")) : new URI((ROOT + locationHeader.get()).replace(" ", "%20"));
 
             if (!(DOMAIN + ":" + PORT).equals(redirectedTo.getAuthority())) {
                 throw new OutsideRedirect("redirect outside of test scope!", redirectedTo);
